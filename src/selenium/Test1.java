8a139fd8849c655193b23274efe7365f94aa9634
BrendrWood <88381169+BrendrWood@users.noreply.github.com>
2021-08-10 15:11:40 +0300

    Запрос Гугл. Работает с Exception-ами exit code -1

--- src/selenium/Test1.java
+++ src/selenium/Test1.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class Test1 {
@@ -18,16 +17,22 @@ public class Test1 {
     private WebDriverWait wait;
 
     @Before
-    public void start(){
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
+    public void start() {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, 10);
+
     }
     @Test
-    public void test(){
-        driver.get("https://www.google.com/");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
+    public void test() {
+        try {
+            driver.get("https://www.google.com/");
+            driver.findElement(By.name("q")).sendKeys("webdriver");
+            driver.findElement(By.name("gNO89b")).click();
+            wait.until(titleIs("webdriver - Поиск в Google"));
+        }
+        catch (org.openqa.selenium.NoSuchElementException e){
+            System.out.println("Поймано исключение, нет элемента");
+        }
     }
     @After
     public void stop(){
