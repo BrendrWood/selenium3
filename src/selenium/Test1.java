f61bbe216e9108379e02abeb77b79a3203e2be3e
BrendrWood <88381169+BrendrWood@users.noreply.github.com>
2021-08-11 17:53:55 +0300

    Запрос Гугл. Работает без Exception. Ловит NoSuchElementException

--- src/selenium/Test1.java
+++ src/selenium/Test1.java
@@ -7,7 +7,13 @@ import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.NoSuchElementException;
+
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -18,25 +24,53 @@ public class Test1 {
 
     @Before
     public void start() {
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-fullscreen");
+            //driver = new ChromeDriver(options);
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability(ChromeOptions.CAPABILITY, options);
+            driver = new ChromeDriver(caps);
             wait = new WebDriverWait(driver, 10);
 
     }
     @Test
-    public void test() {
-        try {
-            driver.get("https://www.google.com/");
-            driver.findElement(By.name("q")).sendKeys("webdriver");
-            driver.findElement(By.name("gNO89b")).click();
-            wait.until(titleIs("webdriver - Поиск в Google"));
+    public void test1() {
+            try {
+                driver.get("https://www.google.com/");
+                driver.findElement(By.name("q")).sendKeys("webdriver");
+                WebElement gNO89b = driver.findElement(By.name("gNO89b"));
+                gNO89b.click();
+                wait.until(titleIs("webdriver - Поиск в Google"));
+            }
+            catch (NoSuchElementException e) {
+            System.out.println("Пойман NoSuchElementException. Элемент gNO89b");
         }
-        catch (org.openqa.selenium.NoSuchElementException e){
-            System.out.println("Поймано исключение, нет элемента");
         }
-    }
+
+
+
+//    @Test
+//    public void test2() {
+//        driver.get("https://www.google.com/");
+//        driver.findElement(By.name("q")).sendKeys("webdriver");
+//        WebElement gNO89b = driver.findElement(By.name("gNO89b"));
+//        gNO89b.click();
+//        wait.until(titleIs("webdriver - Поиск в Google"));
+//    }
+//
+//    @Test
+//    public void test3() {
+//        driver.get("https://www.google.com/");
+//        driver.findElement(By.name("q")).sendKeys("webdriver");
+//        WebElement gNO89b = driver.findElement(By.name("gNO89b"));
+//        gNO89b.click();
+//        wait.until(titleIs("webdriver - Поиск в Google"));
+//    }
+
     @After
     public void stop(){
-        driver.quit();
+        driver.close();
         driver = null;
     }
 }
